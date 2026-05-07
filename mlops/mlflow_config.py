6da99dd25e54120c55900ee4e83dcb083b9a6fe2
Abdo Narjiss <narjiss.abdo@gmail.com>
2026-05-07 12:10:55 +0100

    config: add mlflow configuration

---
+++ mlops/mlflow_config.py
@@ -0,0 +1,17 @@
+import mlflow
+import os
+import logging
+def setup_mlflow(experiment_name="Casa_AirQuality"):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(current_dir)
+
+    db_path = os.path.join(project_root,"mlruns.db")
+    tracking_uri = f"sqlite://{db_path}"
+
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(experiment_name)
+
+    logging.info(f"mlflow connected ! experiment:{experiment_name} | BDD: {db_path}")
+
+    return tracking_uri
+
